873232f63c057220b4c1c431efb1610cb8f9ab50
Josiah Moye <jmoye@neo.rr.com>
2016-07-25 18:38:10 -0600

    Added menu and methods.

--- src/Caesar/Cipher.java
+++ src/Caesar/Cipher.java
@@ -4,29 +4,34 @@ public class Cipher {
 	private int shiftAmount;
 	private String message;
 	private String encryptedMessage = "";
+	private String decryptedMessage = "";
 	
-	public void encrypt() {
+	public void encrypt(String msg, int shift) {
+		message = msg;
+		shiftAmount = shift;
 		for (int i = 0; i < message.length(); i++) {
 			encryptedMessage += (char)(message.charAt(i) + shiftAmount);
 		}
-
-
+		System.out.println(encryptedMessage);
 	}
 	
 	
-	public void decrypt() {
-		
+	public void decrypt(String msg, int shift) {
+		for (int i = 0; i < msg.length(); i++) {
+			decryptedMessage += (char)(msg.charAt(i) - shift);
+		}
+		System.out.println(decryptedMessage);
 	}
-	
-	public void setMessage(String msg) {
-		message = msg;
-	}
-	
-	public void setShift(int shift) {
-		shiftAmount = shift;
-	}
-	public String getEncryptedMessage(){
-		return encryptedMessage;
-	}
-		
+//	
+//	public void setMessage(String msg) {
+//		message = msg;
+//	}
+//	
+//	public void setShift(int shift) {
+//		shiftAmount = shift;
+//	}
+//	public String getEncryptedMessage(){
+//		return encryptedMessage;
+//	}
+//		
 }
--- src/Caesar/CipherApp.java
+++ src/Caesar/CipherApp.java
@@ -6,6 +6,8 @@ public class CipherApp {
 	public static void main(String[] args) {
 		Cipher cipher = new Cipher();
 		int entry = -1;
+		String message = "";
+		int shift = 0;
 		
 		Scanner keyboard = new Scanner(System.in);
 
@@ -17,27 +19,26 @@ public class CipherApp {
 		System.out.println("0 for Encryption");
 		System.out.println("1 for Decryption");
 		entry = keyboard.nextInt();
+		keyboard.nextLine();
 		
 		if (entry == 0){
 			System.out.println("Please enter the message you'd like to encrypt: ");
-			
-			cipher.encrypt();
+			System.out.println();
+			message = keyboard.nextLine();
+			System.out.println("Enter your shift amount: ");
+			shift = keyboard.nextInt();
+
+			cipher.encrypt(message, shift);
+//			System.out.println(cipher.getEncryptedMessage());
 		}
 		if (entry == 1){
 			System.out.println("Please enter the message you'd like to decrypt: ");
-			cipher.decrypt();
+			System.out.println();
+			message = keyboard.nextLine();
+			System.out.println("Enter the shift amount: ");
+			shift = keyboard.nextInt();
+			cipher.decrypt(message, shift);
 		}
-		
-		cipher.setMessage("shitdemon");
-		
-		cipher.setShift(5);
-		
-		cipher.encrypt();
-		
-		System.out.println(cipher.getEncryptedMessage());
-		
-	}
-	public static String keyboardInput(){
 		
 	}
 }
