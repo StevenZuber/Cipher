43cb9e3f4a84827bba920914a28e9f9396edb516
Steve Zuber <stevenjzuber@gmail.com>
2016-07-25 18:05:11 -0600

    Tinkered

--- src/Caesar/Cipher.java
+++ src/Caesar/Cipher.java
@@ -3,19 +3,20 @@ package Caesar;
 public class Cipher {
 	private int shiftAmount;
 	private String message;
-	private String encryptedMessage;
+	private String encryptedMessage = "";
 	
 	public void encrypt() {
-		//String msg;
 		for (int i = 0; i < message.length(); i++) {
 			encryptedMessage += (char)(message.charAt(i) + shiftAmount);
 		}
-		System.out.println(encryptedMessage);
+
+
 	}
 	
-//	public void decrypt() {
-//		
-//	}
+	
+	public void decrypt() {
+		
+	}
 	
 	public void setMessage(String msg) {
 		message = msg;
@@ -24,5 +25,8 @@ public class Cipher {
 	public void setShift(int shift) {
 		shiftAmount = shift;
 	}
-	
+	public String getEncryptedMessage(){
+		return encryptedMessage;
+	}
+		
 }
--- src/Caesar/CipherApp.java
+++ src/Caesar/CipherApp.java
@@ -1,14 +1,43 @@
 package Caesar;
 
+import java.util.Scanner;
+
 public class CipherApp {
 	public static void main(String[] args) {
 		Cipher cipher = new Cipher();
+		int entry = -1;
+		
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.println("\tCAESAR CIPHER");
+		System.out.println("\t_____________");
+		System.out.println();
+		System.out.println("Encryption or Decryption");
+		System.out.println();
+		System.out.println("0 for Encryption");
+		System.out.println("1 for Decryption");
+		entry = keyboard.nextInt();
+		
+		if (entry == 0){
+			System.out.println("Please enter the message you'd like to encrypt: ");
+			
+			cipher.encrypt();
+		}
+		if (entry == 1){
+			System.out.println("Please enter the message you'd like to decrypt: ");
+			cipher.decrypt();
+		}
 		
 		cipher.setMessage("shitdemon");
 		
 		cipher.setShift(5);
 		
 		cipher.encrypt();
+		
+		System.out.println(cipher.getEncryptedMessage());
+		
+	}
+	public static String keyboardInput(){
 		
 	}
 }
